1b80eacd354c3bd51f3500f032492ae4a6f854d6
guicarmaga <126666198+guicarmaga@users.noreply.github.com>
2024-12-05 10:53:53 +0000

    Update teste.py

--- teste.py
+++ teste.py
@@ -8,7 +8,7 @@ from gymnasium.envs.box2d.car_racing import CarRacing
 class CustomCarRacing(CarRacing):
     def __init__(self, render_mode = None):
         super().__init__(render_mode=render_mode)
-        self.wind_force = 0.1
+        self.wind_force = -0.3
 
     def reset(self, *, seed=None, options=None):
         super().reset(seed=seed, options=options)  
@@ -18,7 +18,8 @@ class CustomCarRacing(CarRacing):
         if action is None:
             action = [0.0, 0.0, 0.0]
 
-        action[0] += self.wind_force * np.random.uniform(-1,1)
+        action[0] *= self.wind_force 
+        action[0] = np.random.uniform(-1,1)
         action = np.clip(action,-1,1)
         obs, reward, done, truncated, info = super().step(action)
         
@@ -28,13 +29,41 @@ class CustomCarRacing(CarRacing):
         return obs, reward, done, truncated, info
 
 if __name__ == "__main__":
-    env = CustomCarRacing(render_mode = "rgb_array")
-    check_env(env)
-    obs = env.reset()
+    
+    env = gym.make('CarRacing-v3', render_mode = "human")  
+    env.reset()
 
-    for _ in range(100):
-        action = env.action_space.sample() # random actions
-        obs, reward, done, truncated, info = env.step(action) # excutes and action
-        if done or truncated:
-            obs = env.reset()
-    env.close()
+    model = PPO('MlpPolicy', env, verbose=1)
+    # model = A2C('MlpPolicy', env, verbose=1)
+    model.learn(total_timesteps=10000)
+
+    episodes = 10
+
+    for ep in range(episodes):
+        obs, info = env.reset()
+        done = False
+        while not done:
+            # pass observation to model to get predicted action
+            action, _ = model.predict(obs)
+
+            # pass action to env and get info back
+            obs, rewards, trunc, done, info = env.step(action)
+
+            # show the environment on the screen
+            env.render()
+            print(ep, rewards, done)
+            print("---------------")    
+        
+    
+    
+    # env = CustomCarRacing()
+    # check_env(env)
+    # obs = env.reset()
+
+    # for _ in range(1000):
+    #     action = env.action_space.sample() # random actions
+    #     obs, reward, done, truncated, info = env.step(action) # excutes and action
+
+    #     if done or truncated:
+    #         obs = env.reset()
+    # env.close()
